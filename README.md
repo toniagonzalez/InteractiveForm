10fd3b8db785d086fb3071da1a3f48978e1206b0
Tonia Gonzalez <toniagonzalez@Tonias-Air.hsd1.or.comcast.net>
2019-04-01 12:35:06 -0700

    add jquery and tooltip styling

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # InteractiveForm
+In this project, I JavaScript to enhance an interactive registration form for a fictional conference called "FullStack Conf."
+
+Using the JavaScript jQuery library I added interactivity to supplied HTML to make the form more user-friendly. I also added additional CSS styling.
+
+With jQuery I added customized and conditional behavior and interactivity to validate user input and provide helpful error messages when the user enters invalid information into the form fields.
---
+++ js/script.js
@@ -0,0 +1,23 @@
+//----------Variables--------//
+
+//-----Input Variables
+const name = $('#name');
+const email = $('#mail');
+const creditCard = $('#cc-num');
+const zipCode = $('#zip');
+const cvv = $('#cvv');
+
+//---Name Input Focus Onload
+name.focus();
+
+
+// name.next().css('visibility', 'visible');
+// name.next().text('Hi');
+//
+// email.next().css('visibility', 'visible');
+//
+// creditCard.next().css('visibility', 'visible');
+//
+// zipCode.next().css('visibility', 'visible');
+//
+// cvv.next().css('visibility', 'visible');
